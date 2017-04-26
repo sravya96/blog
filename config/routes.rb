200284cb9f825b1d5421978bf07b43540a64cf40
Sravya Pullagura <sravyaraj96@gmail.com>
2017-04-27 00:11:09 +0530

    Finish styling the welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'welcome/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root "application#hello"
+	root "welcome#new"
 end
