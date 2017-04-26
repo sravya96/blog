cecfce7a213f7b481809598757249a2baccb82d9
Sravya Pullagura <sravyaraj96@gmail.com>
2017-04-27 00:55:16 +0530

    Add form for creating a new article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root "welcome#new"
+ resources :articles 
+
+   root "welcome#new"
+   get "/new", to:"articles#new"
+
 end
